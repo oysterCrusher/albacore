6e824557a51c13c6218781dbad8bbf1bcbd84607
John <j.adams440@gmail.com>
2014-10-02 15:34:53 +0100

    Initial commit. Freshly generated libGDX project.

---
+++ desktop/src/uk/me/jadams/albacore/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package uk.me.jadams.albacore.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import uk.me.jadams.albacore.albacore;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new albacore(), config);
+	}
+}
