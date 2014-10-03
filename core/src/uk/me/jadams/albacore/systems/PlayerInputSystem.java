7fb81f599b851450d1c630ed03a2546ced77c1b4
John <j.adams440@gmail.com>
2014-10-03 15:41:27 +0100

    wasd keys now move player around.

--- core/src/uk/me/jadams/albacore/systems/PlayerInputSystem.java
+++ core/src/uk/me/jadams/albacore/systems/PlayerInputSystem.java
@@ -11,6 +11,8 @@ import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.EntitySystem;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.utils.ImmutableArray;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
@@ -20,13 +22,11 @@ public class PlayerInputSystem extends EntitySystem {
 	private ImmutableArray<Entity> entities;
 	
 	private OrthographicCamera camera;
-	private Input input;
-	
 	private ComponentMapper<PositionComponent> pm = ComponentMapper.getFor(PositionComponent.class);
+	private ComponentMapper<VelocityComponent> vm = ComponentMapper.getFor(VelocityComponent.class);
 
 	public PlayerInputSystem(OrthographicCamera camera, Input input) {
 		this.camera = camera;
-		this.input = input;
 	}
 	
 	@SuppressWarnings("unchecked")
@@ -40,14 +40,18 @@ public class PlayerInputSystem extends EntitySystem {
 	@Override
 	public void update(float deltaTime) {
 		PositionComponent position;
+		VelocityComponent velocity;
+		Vector3 mouseCoords = new Vector3();
 		Vector2 direction = new Vector2();
+		Vector2 move = new Vector2();
 		
 		for (int i = 0; i < entities.size(); i++) {
 			Entity e = entities.get(i);
 			position = pm.get(e);
+			velocity = vm.get(e);
 			
 			// Unproject mouse position to get angle between player position and mouse
-			Vector3 mouseCoords = new Vector3(input.getMouseX(), input.getMouseY(), 0);
+			mouseCoords.set(Gdx.input.getX(), Gdx.input.getY(), 0);
 			camera.unproject(mouseCoords);
 			mouseCoords.sub(position.x, position.y, 0);
 			mouseCoords.nor();
@@ -56,6 +60,26 @@ public class PlayerInputSystem extends EntitySystem {
 			direction.x = mouseCoords.x;
 			direction.y = mouseCoords.y;
 			position.angle = direction.angle();
+			
+			// Add velocity in the direction of the wasd keys
+			move.set(0, 0);
+			if (Gdx.input.isKeyPressed(Keys.A)) {
+				move.x -= 150;
+			}
+			if (Gdx.input.isKeyPressed(Keys.D)) {
+				move.x += 150;
+			}
+			if (Gdx.input.isKeyPressed(Keys.W)) {
+				move.y += 150;
+			}
+			if (Gdx.input.isKeyPressed(Keys.S)) {
+				move.y -= 150;
+			}
+			if (move.len() != 0) {
+				move.nor();
+				velocity.x += move.x * 150;
+				velocity.y += move.y * 150;
+			}
 		}
 	}
 
